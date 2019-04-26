4b8c9fff80373915329e4e48deff73e81d515b94
Dave Cox <davedvst@gmail.com>
2019-04-26 15:05:30 -0700

    final upload maybe

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   mongoURI: "mongodb://localhost/gms_db",
-=======
-  mongoURI: "mongodb://localhost/scraper_news",
->>>>>>> af8a494c7e0a0d1ccd129994359b1ed60599c6a3
   secretOrKey: "secret"
 };
